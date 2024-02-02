f5c31bce8bb5e853ae3734e8fcb8e07a9cedaaf0
AganzeFelicite <aganzefelicite@gmail.com>
2024-02-02 16:52:02 +0200

    home view

--- Monalitics Api/monalytics/monalytics/urls.py
+++ Monalitics Api/monalytics/monalytics/urls.py
@@ -2,10 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from company import urls
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 
+@api_view(['GET'])
 def home(request):
-    return Response({'message': 'welcome to monalytics!'})
+    data = {'message': 'Welcome to Monalytics!'}
+    return Response(data)
 
 
 urlpatterns = [
