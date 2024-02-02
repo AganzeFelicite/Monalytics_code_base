3cd2d2cd6c5d7ec0d2b46eafcb335edb1869fe8b
AganzeFelicite <aganzefelicite@gmail.com>
2024-02-02 16:48:02 +0200

    home view

--- Monalitics Api/monalytics/monalytics/home_view.py
+++
@@ -1,5 +0,0 @@
-from rest_framework.response import Response
-
-
-def home():
-    return Response({'message': 'welcome to monalytics!'})
--- Monalitics Api/monalytics/monalytics/urls.py
+++ Monalitics Api/monalytics/monalytics/urls.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 from company import urls
-from home_view import home
+from rest_framework.response import Response
+
+
+def home(request):
+    return Response({'message': 'welcome to monalytics!'})
+
 
 urlpatterns = [
-    path("", home.as_view()),
+    path("", home),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('monalytics-api/', include(urls)),
