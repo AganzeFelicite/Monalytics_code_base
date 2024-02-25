8369565ff3da2e44362dd0999b89fb00706ef107
AganzeFelicite <aganzefelicite@gmail.com>
2024-02-25 20:26:03 +0200

    cors

--- Monalitics Api/monalytics/monalytics/settings.py
+++ Monalitics Api/monalytics/monalytics/settings.py
@@ -272,8 +272,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-   'django.contrib.auth.middleware.AuthenticationMiddleware',
-
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
