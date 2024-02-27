c330f60049bf1d0ed4a214e698c3751f85a4fa63
AganzeFelicite <aganzefelicite@gmail.com>
2024-02-27 11:36:51 +0200

    fixed compaigns from compaigns

--- Monalitics Api/monalytics/influencer/serializers.py
+++ Monalitics Api/monalytics/influencer/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 from .models import Influencer
+from campaign.serializers import CampaignSerializer
 
 
 class InfluencerSerializer(serializers.ModelSerializer):
     """
     this is a class that defines a influencer serializer
     """
+    campaigns = CampaignSerializer(many=True, read_only=True)
+
     class Meta:
         model = Influencer
         fields = '__all__'
+        read_only_fields = ['id']
